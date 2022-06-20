5edf285296e6f4c3327be5aa0c667ddf040438ec
Nofishtou <damnedbyhimself@gmail.com>
2022-06-20 09:05:00 +0300

    env: added localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { registerLocaleData } from '@angular/common';
+import localeUa from '@angular/common/locales/uk';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+registerLocaleData(localeUa);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	providers: [
+		{
+			provide: LOCALE_ID,
+			useValue: 'uk-UA',
+		},
+		{
+			provide: DEFAULT_CURRENCY_CODE,
+			useValue: 'UAH',
+		},
+		{
+			provide: MAT_DATE_LOCALE,
+			useValue: 'uk-UA',
+		},
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
