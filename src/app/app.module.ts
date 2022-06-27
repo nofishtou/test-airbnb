f1ef892c2713af7ffa9917da83d97a69fec24a21
Nofishtou <damnedbyhimself@gmail.com>
2022-06-27 21:47:00 +0300

    refactor: added locale module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LayoutModule } from './ui/theme/layout/layout.module';
-import { AppLocaleModule } from './app-locale.module';
 
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule, AppLocaleModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule,],
 	providers: [],
 	bootstrap: [AppComponent],
 })
